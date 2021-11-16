59c22eaad4220610c690e7a673561773f8041716
Vlad <tkachenkovladv@gmail.com>
2021-11-16 19:36:35 +0200

    registration - divide main script to main module and module for register user

---
+++ db/users.add_user.js
@@ -0,0 +1,45 @@
+const DATABASE = require('./db');
+const users = require('./users.model');
+
+const addUserToDB = async function createUser(userID, userName, firstName, lastName) {
+    await DATABASE.sync();
+
+    await users.findOrCreate({ // возвращает промис с 2 параметрами: модель и создано (булево)
+        where: {
+            telegram_id: userID
+        },
+        defaults: {  // set the default properties if it doesn't exist
+            username: userName,
+            first_name: firstName,
+            last_name: lastName,
+            telegram_id: userID,
+        }
+    })
+        .then(function (result) {
+            let user = result[0],  //  записываем в переменную вернувшуюся модель (запись юзер)
+                created = result[1]  // записываем в переменную создано или нет
+            if (!created) {
+                console.log(`User ${userName} already registered`);
+            } else {
+                console.log(`User ${userName} created!`)
+            }
+            console.log('фраза в конце скрипта');
+        })
+}
+
+module.exports = addUserToDB;
+
+
+
+// await users.create({  // добавление юзера в базу
+//     username: ctx.from.username,
+//     first_name: ctx.from.first_name,
+//     last_name: ctx.from.last_name,
+//     telegram_id: ctx.from.id,
+// })
+//     .then(ctx.reply('User added to DB'))
+//     .catch((err) => {
+//         console.log(err)
+//         ctx.reply('User NOT added to DB!!!')
+//     })
+
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const { Telegraf, Markup, Composer, Scenes, session } = require('telegraf');
 const fetch = require("node-fetch");
 require('dotenv').config();
 
-const DATABASE = require('./db/db');
-const users = require('./db/users.model');
+const addUserToDB = require('./db/users.add_user')
 
 const constants = require('./const');
 const commands = constants.commandsList;
@@ -91,40 +90,14 @@ bot.command("reg", (ctx) => {
 
 
 bot.hears('auth', async (ctx) => {
-    await DATABASE.sync();
-    // await users.create({  // добавление юзера в базу
-    //     username: ctx.from.username,
-    //     first_name: ctx.from.first_name,
-    //     last_name: ctx.from.last_name,
-    //     telegram_id: ctx.from.id,
-    // })
-    //     .then(ctx.reply('User added to DB'))
-    //     .catch((err) => {
-    //         console.log(err)
-    //         ctx.reply('User NOT added to DB!!!')
-    //     })
 
-    await users.findOrCreate({ // возвращает промис с 2 параметрами: модель и создано (булево)
-        where: {
-            telegram_id: ctx.from.id
-        },
-        defaults: {  // set the default properties if it doesn't exist
-            username: ctx.from.username,
-            first_name: ctx.from.first_name,
-            last_name: ctx.from.last_name,
-            telegram_id: ctx.from.id,
-        }
-    })
-        .then(function (result) {
-            let user = result[0],  //  записываем в переменную вернувшуюся модель (запись юзер)
-                created = result[1]  // записываем в переменную создано или нет
-            if (!created) {
-                console.log(`User ${ctx.from.username} already registered`);
-            } else {
-                console.log(`User ${ctx.from.username} created!`)
-            }
-            console.log('фраза в конце скрипта');
-        })
+    await addUserToDB(
+        ctx.from.id,
+        ctx.from.username,
+        ctx.from.first_name,
+        ctx.from.last_name
+    );
+
 
 })
 
@@ -141,7 +114,6 @@ bot.hears('check', async (ctx) => {
         console.log('---------- finish users ---------')
     });
 });
-
 
 
 
