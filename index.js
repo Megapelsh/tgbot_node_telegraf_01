b868d2d9f256575b19cb74842e3cbddc86534777
Vlad <tkachenkovladv@gmail.com>
2021-11-16 19:04:44 +0200

    registration - check if exist and if not write user to DB

--- index.js
+++ index.js
@@ -92,17 +92,40 @@ bot.command("reg", (ctx) => {
 
 bot.hears('auth', async (ctx) => {
     await DATABASE.sync();
-    await users.create({
-        username: ctx.from.username,
-        first_name: ctx.from.first_name,
-        last_name: ctx.from.last_name,
-        telegram_id: ctx.from.id,
+    // await users.create({  // добавление юзера в базу
+    //     username: ctx.from.username,
+    //     first_name: ctx.from.first_name,
+    //     last_name: ctx.from.last_name,
+    //     telegram_id: ctx.from.id,
+    // })
+    //     .then(ctx.reply('User added to DB'))
+    //     .catch((err) => {
+    //         console.log(err)
+    //         ctx.reply('User NOT added to DB!!!')
+    //     })
+
+    await users.findOrCreate({ // возвращает промис с 2 параметрами: модель и создано (булево)
+        where: {
+            telegram_id: ctx.from.id
+        },
+        defaults: {  // set the default properties if it doesn't exist
+            username: ctx.from.username,
+            first_name: ctx.from.first_name,
+            last_name: ctx.from.last_name,
+            telegram_id: ctx.from.id,
+        }
     })
-        .then(ctx.reply('User added to DB'))
-        .catch((err) => {
-            console.log(err)
-            ctx.reply('User NOT added to DB!!!')
+        .then(function (result) {
+            let user = result[0],  //  записываем в переменную вернувшуюся модель (запись юзер)
+                created = result[1]  // записываем в переменную создано или нет
+            if (!created) {
+                console.log(`User ${ctx.from.username} already registered`);
+            } else {
+                console.log(`User ${ctx.from.username} created!`)
+            }
+            console.log('фраза в конце скрипта');
         })
+
 })
 
 
